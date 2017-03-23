64c8026909075d78b667b2e30ad0562c4e19ab1f
Anurag Tiwari <tiwari.anurag126@gmail.com>
2017-03-23 22:56:13 +0530

    Variables declaration and sample request

--- api/controllers/PaymentController.js
+++ api/controllers/PaymentController.js
@@ -13,14 +13,54 @@ module.exports = {
 		var key = 'tqB2AthR';
 		var salt = 'j5OQnEG6bt';
 
-		var text = "";
+		var txnid = "";
 		var length = 10; // 10 digits random transaction ID.
     	var sample = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     	for(var i = 0; i < length; i++) {
-        	text += sample.charAt(Math.floor(Math.random() * sample.length));
+        	txnid += sample.charAt(Math.floor(Math.random() * sample.length));
     	}
-    	// console.log(text);
-    	
+    	// console.log(txnid);
+
+    	var amount = 10;
+    	var productinfo = 'someinfo';
+    	var firstname = 'Anurag';
+    	var email = 'tiwari.anurag126@gmail.com';
+    	var phone = '9999111111';
+
+    	// The surl and furl are the success and the failure url's. Change accordingly.
+    	var surl = 'http://localhost.com/Payment/success';
+    	var furl = 'http://localhost.com/Payment/failure';
+
+    	// string hash format
+    	var str = key + '|' + txnid + '|' + amount + '|' + productinfo + '|' + firstname + '|' + email + '|' +
+		'||||||||||' + salt;
+		// console.log(str);
+
+		var hash = require('crypto').createHash('sha512').update(str).digest('hex');
+		// console.log(hash);
+
+		var service_provider = 'payu_paisa';
+
+		var formData = {
+			'key' : key,
+			'txnid' : txnid,
+			'amount' : amount,
+			'productinfo' : productinfo,
+			'firstname' : firstname,
+			'email' : email,
+			'phone' : phone,
+			'surl' : surl,
+			'furl' : furl,
+			'hash' : hash,	
+			'service_provider' : service_provider
+		}
+
+		// Note - PayUMoney Server only accepts request in form type data.
+		var request = require('request');
+		request.post('https://secure.payu.in/_payment', {form: formData}, function(err, response, body) {
+			var location = response.caseless.dict.location;
+			res.redirect(location);
+		});
 	}
 };
 
